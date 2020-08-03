07402e628d55d4ea3aadfc5c1ab009f84f16821e
Your Name <jr.vash@gmail.com>
2020-08-03 12:43:18 -0300

    ConclusÃo do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +15,49 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): {} {
+    return { transactions: this.transactions, balance: this.balance };
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  private setBalance(transactions: Transaction[]): Balance {
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    Object.values(transactions).reduce((arrBalance, arrTransaction) => {
+      if (arrTransaction.type === 'income') {
+        balance.income += arrTransaction.value;
+      } else {
+        balance.outcome += arrTransaction.value;
+      }
+
+      return arrBalance;
+    }, 1);
+
+    balance.total = balance.income - balance.outcome;
+
+    this.balance = balance;
+
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    this.setBalance(this.transactions);
+
+    return transaction;
   }
 }
 
